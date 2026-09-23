9c7110b96be3d34a4b9ed33b0369bacb1df2a547
YousifY1 <yyilmaz1700@gmail.com>
2026-09-23 11:46:46 -0700

    Changed lines of code

--- NoSunshine.java
+++ NoSunshine.java
@@ -1,1 +1,38 @@
-public class NoSunshine
+public class NoSunshine {
+
+   public static void main(String[] args) {
+      System.out.println("Ain't no sunshine when she's gone ");
+      System.out.println("It's not warm when she's away ");
+      System.out.println("Ain't no sunshine when she's gone "); 
+      System.out.println("And she's always gone too long ");
+      System.out.println("Anytime she goes away ");
+
+      System.out.println("Wonder this time where she's gone ");
+      System.out.println("Wonder if she's gone to stay ");
+      System.out.println("Ain't no sunshine when she's gone ");
+      System.out.println("And this house just ain't no home ");
+      System.out.println("Anytime she goes away ");
+
+      System.out.println("And I know, I know, I know, I know ");
+      System.out.println("I know, I know, I know, I know, I know ");
+      System.out.println("I know, I know, I know, I know, I know ");
+      System.out.println("I know, I know, I know, I know, I know ");
+      System.out.println("I know, I know, I know, I know, I know ");
+      System.out.println("I know, I know "); 
+
+      System.out.println("Hey, I ought to leave young thing alone ");
+      System.out.println("But ain't no sunshine when she's gone, whoa-whoa ");
+      System.out.println("Ain't no sunshine when she's gone ");
+      System.out.println("Only darkness every day ");
+   
+      System.out.println("Ain't no sunshine when she's gone ");
+      System.out.println("And this house just ain't no home ");
+
+      System.out.println("Anytime she goes away ");
+      System.out.println("Anytime she goes away ");
+      System.out.println("Anytime she goes away ");
+      System.out.println("Anytime she goes away ");
+      }
+}
+      
+      
